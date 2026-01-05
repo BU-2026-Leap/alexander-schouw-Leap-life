e4dee7fd9ca3112f8e1bb071eccf053cd425e029
Alexander Schouw <alexd.schouw@gmail.com>
2026-01-05 16:48:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,32 +12,38 @@ output_filename = "output.json"
 if os.path.exists(output_filename):
     os.remove(output_filename)
 
-average_final = 0.0
-unique_students = 0
+def exam_stats(csv_file):
+    average_final = 0.0
+    unique_students = 0
+    student_list = []
+    total_final_score = 0.0
+    with open(csv_file) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            print(row)
+            if row['exam_name'] == 'final':
+                total_final_score = total_final_score + float(row['score'])
+            if row['student_id'] not in student_list:
+                student_list.append(row['student_id'])
+                student_set = set(student_list)
+                unique_students = len(student_set)
+            average_final = total_final_score / unique_students
+    return {
+        "average_final": average_final,
+        "unique_students": unique_students,
+    }
 
-number_of_finals_count = 0
-total_final_score = 0.0
-
-with open(input_filename) as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        print(row)
-
-        # TODO: compute average final score
-
-        if row['exam_name'] == 'final':
-            number_of_finals_count += 1
-            total_final_score = total_final_score + float(row['score'])
-            unique_students += 1
-        # TODO: unique student count
-
-    average_final = total_final_score / number_of_finals_count
-
-result = {
-    "average_final": average_final,
-    "unique_students": unique_students,
-}
+result = exam_stats(input_filename)
 
 with open(output_filename, "w") as out:
     json.dump(result, out, indent=2)
+
+    # TODO: compute average final score
+
+    # TODO: unique student count
+
+
+
+
+
 
